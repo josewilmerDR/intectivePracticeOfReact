746634b36cf00b5b9ebb50ecdb31ea73c1eed523
josewilmerDR <josewdr@gmail.com>
2023-04-07 20:39:23 +0000

    Varios ejercicios intectivos para aprender React

--- .learn/resets/04.2-conditional-rendering/app.jsx
+++ .learn/resets/04.2-conditional-rendering/app.jsx
@@ -3,14 +3,12 @@ import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
 // add the green inside this function
-const Alert = (props) => {
+const Alert = props => {
 	const colorClasses = {
 		red: "alert-danger",
-		orange: "alert-warning",
-		green: "alert-success",
+		orange: "alert-warning"
 	};
-
-	if (colorClasses[props.color] === undefined) alert(`El color ${props.color} no esta en la lista de color a elegir`);
+	if (colorClasses[props.color] === undefined) alert(`The color ${props.color} is not in the possible list of colors`);
 
 	return (
 		<div className={`alert ${colorClasses[props.color]}`} role="alert">
@@ -20,7 +18,7 @@ const Alert = (props) => {
 };
 Alert.propTypes = {
 	color: PropTypes.string,
-	text: PropTypes.string,
+	text: PropTypes.string
 };
 
 // here is where the alert component is being used, you don't have to edit this part,
